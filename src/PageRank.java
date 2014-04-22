991f9e148222a7705331763870420e2a86c45c62
anersapir <sapir.aner@gmail.com>
2014-04-22 20:44:21 +0300

    dick

--- src/PageRank.java
+++ src/PageRank.java
@@ -1,24 +1,68 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 public class PageRank
 {
 	double[][] A;
 	double[] v;
-	WebGraph wg;
+	WebGraph linksGraph;
 	double epsilon;
+	HashMap<String, Integer> urlToMatrixIndex;
+	HashMap<Integer, String> matrixIndexToUrl;
 	
-	public PageRank(WebGraph wg, double epsilon) 
+	public PageRank(WebGraph linksGraph, double epsilon) 
 	{
-		this.wg = wg;
+		this.linksGraph = linksGraph;
 		this.epsilon = epsilon;
-		this.A = this.initA();
+		this.initMappings();
+		this.initA();
+		
+		this.v = new double[linksGraph.allPages.size()];
+		Arrays.fill(this.v, 1.0);
 	}
 	
-	public double[][] initA()
+	private void initMappings()
 	{
+		this.urlToMatrixIndex = new HashMap<String, Integer>();
+		this.matrixIndexToUrl = new HashMap<Integer, String>();
+
+		//TODO do we have pages in the graph that we didnt crawl on them?
+		int i = 0;
+		
+		for (Page p : this.linksGraph.allPages)
+		{
+			this.urlToMatrixIndex.put(p.URL, i);
+			this.matrixIndexToUrl.put(i, p.URL);
+			i++;
+		}
+
+	}
+	private void initA()
+	{		
+		this.A = new double[this.linksGraph.allPages.size()][this.linksGraph.allPages.size()];
+		
 		//walk on webgraph, fill A
+		for (Page p : this.linksGraph.allPages)
+		{
+			double eachLinkProb = 1 / (p.links.size());
+			
+			for (String link : p.links)
+			{
+				Page temp = new Page(link);
+				if (this.linksGraph.allPages.contains(temp))
+				{
+					this.A[this.urlToMatrixIndex.get(link)][this.urlToMatrixIndex.get(p.URL)] = eachLinkProb;
+				}
+				
+			}
+				
+		}
 	}
 	
-	public double calculateRoundAv()
+	private double calculateRoundAv()
 	{
 		double[] oldV = this.v.clone();//check about deep clone
 		
@@ -26,6 +70,7 @@ public class PageRank
 		for (int i = 0; i < v.length; i++)
 		{
 			double sum = 0;
+			
 			for (int j = 0; j < this.v.length; j++)
 				sum += this.A[i][j]*this.v[j];
 			this.v[i] = sum;
@@ -34,18 +79,51 @@ public class PageRank
 		return calculateDistance(oldV, this.v);
 	}
 	
-	public double calculateDistance(double[] v1, double[] v2)
+	private double calculateDistance(double[] v1, double[] v2)
 	{
+		if (v1.length != v2.length)
+			return -1;
+		
+		int sumOfPows = 0;
+		
+		for (int i = 0; i < v1.length; i++)
+			sumOfPows += (v1[i]-v2[i])*(v1[i]-v2[i]);
+		
+		return Math.sqrt(sumOfPows);
 	}
 	
 	public void findPR()
 	{
-		double distance = INF;
-		while distance >= epsilon
+		double distance = Double.POSITIVE_INFINITY;
+		
+		while (distance >= this.epsilon)
 		{
 			distance = calculateRoundAv();
 		}
 	}
 	
+	public LinkedHashMap<String, Double> getUrlsWithRanksOrderbyRank()
+	{
+		TreeMap<String, String> rankToUrlMapping = new TreeMap<String, String>(new LetMeHaveDuplicateDoubleKeysComparator());
+		LinkedHashMap<String, Double> urlToRankMappingOrderbyRank = new LinkedHashMap<String, Double>();
+		HashMap<Double, Integer> currInstanceOfRank = new HashMap<Double, Integer>();
+		
+		for (int i = 0; i < this.v.length; i++)
+		{
+			if (!currInstanceOfRank.containsKey(this.v[i]))
+				currInstanceOfRank.put(this.v[i], 1);
+			else
+				currInstanceOfRank.put(this.v[i], currInstanceOfRank.get(this.v[i]) + 1);
+			
+			String keyWithInstanceCounting = String.valueOf(this.v[i]) + '#' + String.valueOf(currInstanceOfRank.get(this.v[i]));
+			rankToUrlMapping.put(keyWithInstanceCounting, this.matrixIndexToUrl.get(i));
+		}
+		
+		// reverse the key-value while keeping order (DESC sorted by the original key == rank)
+		for (Entry<String, String> rankToUrl : rankToUrlMapping.descendingMap().entrySet())
+			urlToRankMappingOrderbyRank.put(rankToUrl.getValue(), Double.valueOf(rankToUrl.getKey().split("#")[0]));
+			
+		return urlToRankMappingOrderbyRank;
+	}
 }
 
--- src/Search.java
+++ src/Search.java
@@ -4,10 +4,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,7 +26,7 @@ public class Search
 	private Queue<String> crawlingQueue = new LinkedList<String>();
 	private Set<String> alreadyInQueue = new TreeSet<String>();
 
-	
+
 	public Search(String startingURL)
 	{
 		this.startingURL = startingURL;
@@ -29,56 +35,160 @@ public class Search
 		this.crawlingQueue = new LinkedList<String>();
 		this.alreadyInQueue = new TreeSet<String>();
 	}
-	
-	public void crawl(int crawlPagesLimit)
+
+
+	public TreeMap<String, String> TA(ArrayList<LinkedHashMap<String, Double>> rankMaps, int k)
 	{
-		int counter = 0;
-		String nextCrawlURL = this.startingURL;
-		this.crawlingQueue.add(nextCrawlURL);
-		
-		while ((nextCrawlURL = crawlingQueue.poll()) != null && counter <= crawlPagesLimit )
+		/*
+		// can implement comparator inside the class (?) we did it in soft1...(?)
+		class IdDataStruct implements Comparator<IdDataStruct>
 		{
-			System.out.println(nextCrawlURL);
-			if (!alreadyInQueue.contains(nextCrawlURL))
+			private Double scoreAfterAggregation;
+			private boolean inTopK;
+
+			public int compare(IdDataStruct id1, IdDataStruct id2) 
 			{
-				String content = fetchPage(nextCrawlURL);
-				
-				if (!content.isEmpty())
-				{
-					Page p = new Page(nextCrawlURL);
-					this.linksGraph.addNewPage(p);
-					addLinksToQueueAndToPage(content, p);
-					// 18.004
-					//this.linksGraph.allPages.add(p);
-					
-					PageWordsParser parser = new PageWordsParser(nextCrawlURL, content);
-					parser.extractWords();
-					parser.calculateScores();
-					parser.addWordsAndScoresToInvertedIndex(this.words);
-				}
-				this.alreadyInQueue.add(nextCrawlURL);
-				counter++;
+				return id1.scoreAfterAggregation.compareTo(id2.scoreAfterAggregation);
 			}
 		}
-	}
-	public static void main(String[] args)
-	{
-		Search s = new Search("http://simple.wikipedia.org/wiki/Albert_einstein");
-		s.crawl(20);
-		//s.linksGraph.allPages
-		System.out.println("blalalaal");
-	}
+		 */
 
+		ArrayList<Iterator<Entry<String, Double>>> rankMapIteratorArr = new ArrayList<Iterator<Entry<String, Double>>>(rankMaps.size());
+		for (LinkedHashMap<String, Double> rankMap : rankMaps)
+			rankMapIteratorArr.add(rankMap.entrySet().iterator());
 
-	public void addLinksToQueueAndToPage(String content, Page p)
-	{
-		String pattern = "(href=\"(\\/wiki\\/.*?)\")";
-		Pattern pat = Pattern.compile(pattern);
-		Matcher m = pat.matcher(content);
-		
-		while (m.find())
+		HashMap<Integer, Entry<String, Double>> minPerListInSortedAccess = new HashMap<Integer, Entry<String, Double>>(rankMaps.size());
+
+		double threshold = Double.POSITIVE_INFINITY;
+
+		TreeMap<String, String> topK = new TreeMap<String, String>(new LetMeHaveDuplicateDoubleKeysComparator());
+		HashMap<Double, Integer> currInstanceOfTotalRank = new HashMap<Double, Integer>();
+
+		// Double[2] : [0] => score, [1] => is in topK
+		HashMap<String, Boolean> seenIdsDecisions = new HashMap<String, Boolean>();
+
+		while ((topK.size() < k) || (Double.valueOf(topK.firstKey().split("#")[0]) < threshold))
 		{
-			String url = "http://simple.wikipedia.org/" + m.group(2);
+			// round of "parallel" sorted access to all rank lists
+			for (int i = 0; i < rankMaps.size(); i++)
+			{
+				Entry<String, Double> currMinItem = minPerListInSortedAccess.get(i);
+				Entry<String, Double> nextItemWithLowerScore = null;
+
+				Iterator<Entry<String, Double>> rankListIterator = rankMapIteratorArr.get(i);
+				if (rankListIterator.hasNext())
+				{
+					// walk down on the i-th ranks list
+					nextItemWithLowerScore = rankListIterator.next();
+					// update the lists minimum items hash table
+					minPerListInSortedAccess.put(i, nextItemWithLowerScore);
+
+					// if item not already seen
+					if (!seenIdsDecisions.containsKey(nextItemWithLowerScore.getKey()))
+					{
+						// now its seen
+						seenIdsDecisions.put(nextItemWithLowerScore.getKey(), false);
+
+						// calculate total rank
+						double total = 0;
+						// add to topK if there aren't K items yet OR if its score is better than the lowest score
+
+						boolean lessthanK = (topK.size() < k);
+						if (lessthanK || (Double.valueOf(topK.firstKey().split("#")[0]) < total) )
+						{
+							// remove lowest item (we're replacing it with the better-total item)
+							if (!lessthanK)
+								topK.pollFirstEntry();
+
+							if (!currInstanceOfTotalRank.containsKey(total))
+								currInstanceOfTotalRank.put(total, 1);
+							else
+								currInstanceOfTotalRank.put(total, currInstanceOfTotalRank.get(total + 1));
+
+							String keyWithInstanceCounting = String.valueOf(total) + '#' + String.valueOf(currInstanceOfTotalRank.get(total));
+							topK.put(keyWithInstanceCounting, nextItemWithLowerScore.getKey());
+
+							// mark that it's taken
+							seenIdsDecisions.put(nextItemWithLowerScore.getKey(), true);
+
+							// update threshold
+							threshold = updateThreshold(threshold, rankMaps.size(), i, currMinItem.getValue(), nextItemWithLowerScore.getValue());
+						}
+					}
+
+				}
+
+				/* we've reached the end of the rank list     threshold ? and what else */
+				else
+				{
+
+				}
+
+
+			}
+			/* there's some item that is the "lowest" in its rank list and it haven't been taken to the topK */
+			/*else
+				{
+
+				}*/
+		
+		}
+		return topK;
+}
+
+// aggregation func: [pagerank + avg(each word rank)] / 2 
+public double updateThreshold(double threshold, int numOfRankMaps, int rankListIndexThatChanged, double oldVal, double newVal)
+{
+	// speciefic
+	if (rankListIndexThatChanged == 0) //pagerank!
+		return (threshold - oldVal/2 + newVal/2);
+	else
+		return (threshold - oldVal/(2*(numOfRankMaps-1)) + newVal/(2*(numOfRankMaps-1)));
+}
+
+public void crawl(int crawlPagesLimit)
+{
+	int counter = 0;
+	String nextCrawlURL = this.startingURL;
+	this.crawlingQueue.add(nextCrawlURL);
+
+	while ((nextCrawlURL = crawlingQueue.poll()) != null && counter <= crawlPagesLimit )
+	{
+		System.out.println(nextCrawlURL);
+		if (!alreadyInQueue.contains(nextCrawlURL))
+		{
+			String content = fetchPage(nextCrawlURL);
+
+			if (!content.isEmpty())
+			{
+				Page p = new Page(nextCrawlURL);
+				this.linksGraph.addNewPage(p);
+				addLinksToQueueAndToPage(content, p);
+				// 18.004
+				//this.linksGraph.allPages.add(p);
+
+				PageWordsParser parser = new PageWordsParser(nextCrawlURL, content);
+				parser.extractWords();
+				parser.calculateScores();
+				parser.addWordsAndScoresToInvertedIndex(this.words);
+			}
+			this.alreadyInQueue.add(nextCrawlURL);
+			counter++;
+		}
+	}
+}
+
+public void addLinksToQueueAndToPage(String content, Page p)
+{
+	String pattern = "(href=\"(\\/wiki\\/.*?)\")";
+	Pattern pat = Pattern.compile(pattern);
+	Matcher m = pat.matcher(content);
+
+	while (m.find())
+	{
+		String url = "http://simple.wikipedia.org/" + m.group(2);
+		if (url != null)
+		{
 			p.links.add(url);
 			if (!alreadyInQueue.contains(url))
 			{
@@ -87,42 +197,54 @@ public class Search
 			}
 		}
 	}
+}
 
-	public String fetchPage(String urlAddress)
-	{
-		URL url;
-		InputStream inputStream = null;
-		BufferedReader bufferedReader;
-		String content="";
-		String line;
-		try {
-			url = new URL(urlAddress);
-			inputStream = url.openStream();  // throws an IOException
-			bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-			while ((line = bufferedReader.readLine()) != null) {
-				content+=line;
-			}
+public String fetchPage(String urlAddress)
+{
+	URL url;
+	InputStream inputStream = null;
+	BufferedReader bufferedReader;
+	String content="";
+	String line;
+	try {
+		url = new URL(urlAddress);
+		inputStream = url.openStream();  // throws an IOException
+		bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+		while ((line = bufferedReader.readLine()) != null) {
+			content+=line;
 		}
-		catch (MalformedURLException mue) 
+	}
+	catch (MalformedURLException mue) 
+	{
+		mue.printStackTrace();
+	}
+	catch (IOException ioe) 
+	{
+		ioe.printStackTrace();
+	} 
+	finally 
+	{
+		try 
 		{
-			mue.printStackTrace();
+			if (inputStream != null) inputStream.close();
 		}
 		catch (IOException ioe) 
 		{
 			ioe.printStackTrace();
-		} 
-		finally 
-		{
-			try 
-			{
-				if (inputStream != null) inputStream.close();
-			}
-			catch (IOException ioe) 
-			{
-				ioe.printStackTrace();
-			}
-
 		}
-		return content;
+
 	}
+	return content;
+}
+
+public static void main(String[] args)
+{
+	Search s = new Search("http://simple.wikipedia.org/wiki/Albert_einstein");
+	s.crawl(20);
+	//s.linksGraph.allPages
+	PageRank pr = new PageRank(s.linksGraph, 1);
+	pr.findPR();
+	System.out.println("blalalaal");
+}
+
 }
